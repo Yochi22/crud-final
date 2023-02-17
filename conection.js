be5058c0869b891e3d8d429bf905b2587dcae1ab
karen1278 <kjaneth111@gmail.com>
2023-02-16 20:28:33 -0500

    new commit

--- conection.js
+++ conection.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://yochi22:Marcador22@cluster0.jzbl6wm.mongodb.net/videoclub')
+mongoose.connect('mongodb+srv://yochi22:Marcador22@cluster0.jzbl6wm.mongodb.net/videoclub');
+
+const objetobd = mongoose.connection
+
+
+objetobd.on('connected', ()=>{
+    console.log('conexion xorrecta a mongo')
+})
+objetobd.on('error', ()=>{
+    console.log('error mongo')
+})
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express =require ("express")
 const app = express()
 
+const archivodb = require('./conection')
+
 app.get('/', (req, res)=>{
     res.end('bienvenidos')
 })
