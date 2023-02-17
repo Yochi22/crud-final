72c3e494016e8b7b921f568d81883f488378fe37
karen1278 <kjaneth111@gmail.com>
2023-02-16 19:42:38 -0500

    conection database

---
+++ conection.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://yochi22:Marcador22@cluster0.jzbl6wm.mongodb.net/videoclub')
--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const express =require ("express")
 const app = express()
+
+app.get('/', (req, res)=>{
+    res.end('bienvenidos')
+})
+
+app.listen(5000, function(){
+    console.log("el serv corre correctamente")
+})
